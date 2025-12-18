9c735f2863a9a153ef848e5b40bfdd1255c82d2b
soumyaranjandas24 <soumyaranjan24@outlook.com>
2025-12-18 10:38:24 -0500

    Front page completed

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
                 <div className="heading-side">
                     <h1 className="heading-title">Conference Event Planner</h1>
                     <p className="heading-description">Plan your next major event with us!</p>
-                    <button onClick={() => handleGetStarted}>Get Started</button>
+                    <button id='get-started-btn' onClick={() => handleGetStarted}>Get Started</button>
                 </div>
                 <div className="main-content">
                     <AboutUs />
